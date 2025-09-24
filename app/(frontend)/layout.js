df28449217fa569c568e38a2b9d1bbb63c9f8a91
5ovit <5ovit@users.noreply.github.com>
2025-09-25 01:25:09 +0530

    Just a minor change in the layouts

--- app/(frontend)/layout.js
+++ app/(frontend)/layout.js
@@ -1,43 +1,14 @@
-import { Roboto } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  fallback: ["Helvetica", "Arial", "sans-serif"],
-  variable: "--font-roboto",
-});
-
-const redwood = localFont({
-  src: "../../public/fonts/Realwood Regular.otf",
-  variable: "--font-redwood",
-})
-
-export const metadata = {
-  title: {
-    template: "%s | Saranda IITM BS",
-    default: "Saranda House | IITM BS",
-  },
-  description: "Saranda, a house at IIT Madras BS program, fosters creativity and innovation through meetups, workshops, tech showcases, eSports, and cultural events.",
-  authors: [{ name: "Sovit", url: "https://www.linkedin.com/in/5ovit/" }],
-  creator: "Saranda WebOps Team",
-  keywords: [
-    'Saranda', 'House', 'IIT Madras', 'BS Program', 'Community', 'Meetups',
-    'Events', 'Workshops', 'eSports', 'Cultural', 'Tech', 'Students'
-  ],
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${roboto.variable} ${redwood.variable}`}>
-      <body>
-        <Header/>
-        {children}
-        <Footer/>
-      </body>
-    </html>
+    <body className="bg-neutral-light">
+      <Header/>
+      {children}
+      <Footer/>
+    </body>
   );
 }
---
+++ app/layout.js
@@ -0,0 +1,40 @@
+import { Roboto } from "next/font/google";
+import localFont from "next/font/local";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+  variable: "--font-roboto",
+});
+
+const redwood = localFont({
+  src: "../public/fonts/Realwood Regular.otf",
+  variable: "--font-redwood",
+})
+
+export const metadata = {
+  title: {
+    template: "%s | Saranda IITM BS",
+    default: "Saranda House | IITM BS",
+  },
+  description: "Saranda, a house at IIT Madras BS program, fosters creativity and innovation through meetups, workshops, tech showcases, eSports, and cultural events.",
+  authors: [
+    {
+      name: "Sovit",
+      url: "https://www.linkedin.com/in/5ovit/",
+    },
+  ],
+  creator: "Saranda WebOps Team",
+  keywords: [
+    'Saranda', 'House', 'IIT Madras', 'BS Program', 'Community', 'Meetups',
+    'Events', 'Workshops', 'eSports', 'Cultural', 'Tech', 'Students'
+  ],
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${roboto.variable} ${redwood.variable}`}>
+      {children}
+    </html>
+  );
+}
