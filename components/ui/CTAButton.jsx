24e0440bcf8f7ced72c6c96bda2c7edd5044ba99
5ovit <5ovit@users.noreply.github.com>
2025-09-26 02:29:52 +0530

    Added a border in the CTAButton

--- components/ui/CTAButton.jsx
+++ components/ui/CTAButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function CTAButton({href="#", children}) {
   return <Link href={href} className="
     bg-neutral-light text-primary rounded-lg px-[0.5rem] py-[0.4rem]
-    hover:bg-secondary active:bg-secondary
+    hover:bg-secondary active:bg-secondary border-1 border-primary
   ">
     <button>{children}</button>
   </Link>
